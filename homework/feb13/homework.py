621ebdea969b278397f684ec3d1d392c355f01f8
TBTGdoesthegithub <77811483+TBTGdoesthegithub@users.noreply.github.com>
2021-03-02 08:35:15 -0500

    Update homework.py

--- homework/feb13/homework.py
+++ homework/feb13/homework.py
@@ -15,16 +15,21 @@ printing, calculations, and variables
 """
 
 # 1: In one sentence, please explain what a computer program is
+# A computer program is lines  of instructions that tell a computer what to do.
 
 # 2: What is the difference between SOFTWARE and HARDWARE?
+# Software is a program that runs on the computer, and hardware is the physical parts of a computer
 
 # 3: Below, write a line of code that will print your name
+print("Evan")
 
 # 4: Below, write some code that will print this:
 """
 ^  ^
 ____
 """
+print("^  ^\n____")
+
 # 5: Below, write some code that will print this:
 """
  ___
@@ -32,28 +37,36 @@ ____
 |+ ;|
 `---'
 """
+print(" ___\n|[_]|\n|+ ;|\n`---'")
+
 # 6: In python what type of symbol is '*'?
+# Multiplication
 
 # 7: What is the output of the following code? (in other words, what the code will print)
 print(6 + 6 * 2 + 3)
+# 21
 
 # 8: What is the output of the following code?
 print(44/(4 + 7) * 2)
+# 8
 
 # 9: What is the output of the following code?
 print((2 + 2) * (2 + 2) / (2 + 2))
+# 4
 
 # 10: What is the output of the following code?
 x = 3
 y = 120
 y = y + x
 print(y)
+# 123
 
 # 11: What is the output of the following code?
 found_coins = 5
 magic_coins = 3
 stolen_coins = 5
 print((found_coins + stolen_coins) * magic_coins)
+# 30
 
 # 12: What is the output of the following code?
 x = 1
@@ -62,6 +75,7 @@ y = x + y
 john = y + x
 x = john
 print(john + x + y)
+# 11
 
 # You are all done! Yay!
 # --- FINISHED ---
